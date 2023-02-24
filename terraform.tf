ddf76bbe01019d606b48e9eff8d65631b8d91e9d
talyoffe <tyoffeawscloud@gmail.com>
2023-02-24 12:06:56 -0500

    added terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,21 @@
+terraform {
+
+ /* cloud {
+    organization = "tal-org-1"
+
+    workspaces {
+      name = "learn-terraform-cloud"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.28.0"
+    }
+  }
+  */
+
+  required_version = ">= 0.14.0"
+}
+
